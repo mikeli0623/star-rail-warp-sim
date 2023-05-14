b89e82b3bc2e9ba6863d3e58d0baaa834e059922
lim211 <lim211@mcmaster.ca>
2023-05-14 17:36:42 -0400

    Initial menu commit

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import { json, allChars, allWeapons } from "./classes/Constants";
 import WarpSingle from "./components/WarpSingle";
 import History from "./classes/History";
 import MiniBanners from "./components/MiniBanners";
+import Settings from "./components/Settings";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 
 function App() {
@@ -244,6 +245,7 @@ function App() {
               title="View source code"
             />
           </a>
+          <Settings resize={resize} />
           <div id="main-back-cover" />
           <div
             id="main-back-cover-pattern"
