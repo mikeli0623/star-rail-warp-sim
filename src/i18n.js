a3b0907b677f9df8497d65cd9cacfeff0b28b291
lim211 <lim211@mcmaster.ca>
2023-06-02 18:35:57 -0400

    Turned off i18n debug messages

--- src/i18n.js
+++ src/i18n.js
@@ -16,7 +16,7 @@ i18n
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    debug: true,
+    debug: false,
     fallbackLng: "en",
     interpolation: {
       escapeValue: false, // not needed for react as React escapes by default
