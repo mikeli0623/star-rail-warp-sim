babe7e606fa8170f61912c2a710cef80dfba1deb
mikeli0623 <mike.li0623@gmail.com>
2025-04-07 01:59:28 -0400

    bugfix/commit-messages

--- src/components/modals/StartModal.jsx
+++ src/components/modals/StartModal.jsx
@@ -36,8 +36,8 @@ export default function StartModal({ show, setShow }) {
         let i = 0;
         while (list.length < 5) {
           if (
-            !data[i].commit.message.includes("Merge pull request") ||
-            !data[i].commit.message.includes("chore") ||
+            !data[i].commit.message.includes("Merge pull request") &&
+            !data[i].commit.message.includes("chore") &&
             !data[i].commit.message.includes("bugfix")
           )
             list.push(data[i].commit);
